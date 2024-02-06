b855c1f9eb7a326ff501005a1f1890479a87d336
Kazakov Igor <nejer777@gmail.com>
2024-02-06 20:35:15 +0600

    Added topic5

---
+++ topic5/classwork.py
@@ -0,0 +1,19 @@
+word = "Тося"
+
+for c in word:
+    print(c)
+
+print()
+
+for i in range(len(word)):
+    print(word[i])
+
+#word = 'Лутший'
+#word_list = list(word)
+#word_list[2] = 'ч'  # исправим ошибку, заменив элемент списка другим символом
+#print(word_list)
+
+#word = str(word_list)  # попытка объединить список символов в строку
+#print(word)  # вывод полученного объекта
+#word = ''.join(word_list)  # использование метода строк "join" для объединения списка символов в строку
+#print(word)  # вывод полученного объекта
